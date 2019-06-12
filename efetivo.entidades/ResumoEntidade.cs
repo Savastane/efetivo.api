c504b67d804833c6e923fbb3b714278608c1ea50
Savastane Santana de Andrade <savastane@hotmail.com>
2019-06-12 15:43:26 -0300

    teste

--- efetivo.entidades/ResumoEntidade.cs
+++ efetivo.entidades/ResumoEntidade.cs
@@ -7,6 +7,7 @@ namespace efetivo.entidades
     public class ResumoEntidade
     {
         public long Id { get; set; }
+        public long Id1 { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
     }
